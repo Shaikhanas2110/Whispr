5e50d7e4f8464c2ecdd2edaaee6db94f898da526
Anas Shaikh <shaikhanas2354@gmail.com>
2026-06-27 03:30:53 +0530

    unused import deleted

--- lib/features/feed/presentation/feed_screen.dart
+++ lib/features/feed/presentation/feed_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:go_router/go_router.dart';
-// import 'package:rive/rive.dart';
 import '../data/feed_provider.dart';
 import '../../post/presentation/post_card.dart';
 import '../../../app/constants.dart';
