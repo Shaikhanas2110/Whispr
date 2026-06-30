aea0edbed59a2a246b9e1ca78c1c7e8f4b08cd42
Anas Shaikh <shaikhanas2354@gmail.com>
2026-07-01 02:02:56 +0530

    Unused import statement deleted!

--- lib/features/feed/presentation/home_shell.dart
+++ lib/features/feed/presentation/home_shell.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:rive/rive.dart';
 import '../../notifications/data/notification_service.dart';
 
 class NewPalette {
